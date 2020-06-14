e7bcdc09b44941da2c1db350421af467feaa0cdd
gheezl <gheezl25@gmail.com>
2020-06-13 22:16:22 -0400

    okay

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -6,7 +6,7 @@ const Pokemon = ({ getPokemon }) => {
     return (
         <Fragment>
             <div>
-                <h1 onClick={getPokemon} >Hello World</h1>
+                <h1 onClick={getPokemon} ></h1>
             </div>
         </Fragment>
     )
