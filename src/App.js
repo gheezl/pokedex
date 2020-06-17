37e932aff520e3a01e1dcbc08a7516633ddc9208
gheezl <gheezl25@gmail.com>
2020-06-16 20:51:18 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import Header from "./components/Nav/Header.js";
 import Pokemon from "./components/pokemon/Pokemon.js";
+import axious from "axios";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      currentPokemon: "",
+      currentPokemonID: "",
     }
   }
 
+  componentDidMount = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then(response => response.json())
+      .then(pokemon => this.setState({ currentPokemon: pokemon.name, currentPokemonID: pokemon.order }))
+    console.log(this.state.currentPokemon)
+    console.log(this.state.currentPokemonID)
+  }
+
   getPokemon = () => {
-    fetch("https://pokeapi.glitch.me/v1/pokemon/:pikachu")
-      .then(response => console.log(response))
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then(response => response.json())
+      .then(pokemon => console.log(pokemon))
+    console.log(this.state.currentPokemonID)
   }
 
   render() {
@@ -21,7 +33,7 @@ class App extends Component {
       <Fragment>
         <Header />
         <div>
-          <Pokemon getPokemon={this.getPokemon} />
+          <Pokemon getPokemon={this.getPokemon} pokemonInfo={this.state.currentPokemon} pokemonId={this.state.currentPokemonID} />
           <Pokemon getPokemon={this.getPokemon} />
         </div>
       </Fragment>
--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -2,12 +2,12 @@ import React, { Fragment } from 'react';
 import "./Pokemon.css"
 
 
-const Pokemon = ({ getPokemon }) => {
+const Pokemon = ({ getPokemon, pokemonInfo, pokemonId }) => {
     return (
         <Fragment>
             <div onClick={getPokemon} className="card">
-                <h2 className="name">Pikachu #321</h2>
-                <h3 className="species">species</h3>
+                <h2 className="name"> {pokemonInfo} #{pokemonId} </h2>
+                <h3 className="species"> </h3>
             </div>
         </Fragment>
     )
